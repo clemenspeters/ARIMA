bad4bb377db9bc39dc699e60db21119a487d268c
Clemens Peters <clemens.peters@honestfoodcompany.de>
2019-09-21 15:30:04 +0200

    Allows for multiple anomalies in anomalyDetector.

--- anomalyDetector.py
+++ anomalyDetector.py
@@ -1,50 +1,76 @@
 from sklearn.manifold import TSNE
+import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 plt.style.use('ggplot')
 
 
 class AnomalyDetector:
-    def __init__(self):
-        self.windowSize = 50
-        self.stride = self.windowSize / 2
+    def __init__(self, window_size, anomalies):
+        self.anomalies = anomalies
+        self.window_size = window_size
+        self.stride = self.window_size / 2
         self.features = []
+        self.window_labels = []
 
-    def reduceArma(self, data):
-        windows = self.getWindows(data)
-        self.features = self.getParameters(windows, self.getArmaParams)
+    def reduce_arma(self, data):
+        windows = self.get_windows(data)
+        self.features = self.get_parameters(windows, self.get_arma_params)
+        self.save_data(self.features)
 
-    def getWindows(self, data):
+    def get_windows(self, data):
         result = []
         start = 0
-        end = self.windowSize
+        end = self.window_size
         while (end <= data.size):
+            self.window_labels.append([start, end])
             result.append(data[start: end])
             start = int( start + self.stride)
             end = int( end + self.stride)
         return result
 
-    def getParameters(self, windows, encoderFunction):
+    def get_parameters(self, windows, encoder_function):
         parameters = []
         for window in windows:
-            params = encoderFunction(window)
+            params = encoder_function(window)
             parameters.append(params)
         return parameters
 
-    def getArmaParams(self, window):
-        model = sm.tsa.ARMA(window, (2, 2)).fit(trend='nc', disp=0)
-        return model.params
+    def get_arma_params(self, dataWindow):
+        model = sm.tsa.ARMA(dataWindow, (2, 2))
+        startParams=[.75, -.25, .65, .35] # Manual hack to avoid errors
+        result = model.fit(trend='nc', disp=0, start_params=startParams)
+        # result = model.fit(trend='nc', disp=0)
+        return result.params
+    
+    def save_data(self, data):
+        """Write data to features.npy file.
+        """
+        np.save('features', data)
 
-    def printFeatures(self):
+    def load_data(self):
+        """Load data from features.npy file.
+        """
+        return np.load('features.npy')
+
+    def print_features(self):
         for feature in self.features:
             print(feature)
 
-    def visualizeFeatures(self, method='TSNE'):
+    def visualize_features(self, method='TSNE'):
         if (method == 'TSNE'):
             embedded = TSNE(n_components=2).fit_transform(self.features)
             fig = plt.figure(1, figsize=(12, 3))
             sub1 = fig.add_subplot(111)
             sub1.scatter(embedded[:, 0], embedded[:, 1])
+             # Add labels and color to anomaly datapoints
+            for anomaly in self.anomalies:
+                start = anomaly * self.window_size
+                end = (anomaly + 1) * self.window_size
+                for i, txt in enumerate(self.window_labels):
+                    if (txt == [start, end]):
+                        sub1.scatter(embedded[i, 0], embedded[i, 1], c='green')
+                        sub1.annotate('Anomaly: {}'.format(txt), (embedded[i, 0], embedded[i, 1]))
             fig.savefig('TSNE-features.png')
             # Show the plot in non-blocking mode
             plt.show()
--- anomalyTester.py
+++ anomalyTester.py
@@ -1,15 +1,25 @@
+import numpy as np
 import dataGenerator
 import anomalyDetector
 
-# Generate data
-windowSize = 100
-windowCount = 10
-anomalies = [5, 7]
-generator = dataGenerator.DataGenerator(windowCount, windowSize, anomalies)
-data = generator.generate_data(False)
+def load_data(filename):
+    """Load data from file."""
+    return np.load(filename)
 
-# Generate features
-# detector = anomalyDetector.AnomalyDetector(anomaly)
-# detector.reduceArma(data)
-# detector.printFeatures()
-# detector.visualizeFeatures()
+def generate_features(data, window_size, anomalies):
+    # Generate features
+    detector = anomalyDetector.AnomalyDetector(window_size, anomalies)
+    detector.reduce_arma(data)
+    detector.visualize_features()
+
+window_size = 100
+window_count = 1000
+anomalies = [500]
+plot_data = True
+generator = dataGenerator.DataGenerator(window_count, window_size, anomalies)
+
+# data = generator.generate_data(plot_data)
+data = load_data('generated_data.npy')
+# generator.visualize(data)
+
+generate_features(data, window_size, anomalies)
