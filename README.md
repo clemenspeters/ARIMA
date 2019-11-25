7662058d242074909e2e980b82cf7883be89f7aa
Clemens Peters <mail@clemenspeters.de>
2019-11-25 02:26:23 +0100

    :recycle: Refactors taxi script to use new data file structure.

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ rather than a production ready perfect project right now.
 
 ## Autoencoder
 
+![Autoencoder](./results/generated/autoencoder/anomaly_scores_regularization_0_0001_model.png)
+
 Example of anomaly detection using generated timeseries data and autoencoder.
 The example show results for different regularization values.
 
@@ -15,12 +17,14 @@ python3 autoencoderExample.py | tee autoencoder/log.txt
 
 ## NYC Taxi Dataset
 
+![NYC Taxi Dataset Numenta](./results/taxi_nyc_numenta_data/taxi_data.png)
+
 The dataset is downloaded from [Numenta](https://github.com/numenta/NAB/blob/master/data/realKnownCause/nyc_taxi.csv).  
 The raw data is from the [NYC Taxi and Limousine Commission](https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page).  
 For other datasets please also see [The Numenta Anomaly Benchmark](https://github.com/numenta/NAB).
 
 ```console
-python3 taxi_nyc_numenta_data.py | tee taxi_nyc_numenta_data/log_taxi.txt
+python3 taxi_nyc_numenta_data.py | tee results/taxi_nyc_numenta_data/log_taxi.txt
 ```
 
 Please find results in ./taxi_nyc_numenta_data.
@@ -38,6 +42,8 @@ python3 yahoo.py | tee log_yahoo.txt
 ```
 
 ## Generated times series
+
+![Generated times series](./results/generated/generated_data_zoomed.png)
 
 Two time series are generate from two ARMA models with different parameters.  
 Then they are combined into one time series which contains anomalies.
--- taxi_nyc_numenta_data.py
+++ taxi_nyc_numenta_data.py
@@ -5,6 +5,7 @@ import dataProcessor
 import anomalyDetector
 import utils
 import matplotlib.pyplot as plt
+import terminalColors as tc
 plt.style.use('ggplot')
 
 
@@ -17,6 +18,7 @@ plt.style.use('ggplot')
     "2015-01-27 00:00:00.000000", 10082,        10080,                  201, 200
 
 """
+folder = 'results/taxi_nyc_numenta_data'
 
 def visualize(data, anomalies, title):
     """Plot the generated (stitched) data containing the anomalies.
@@ -26,37 +28,46 @@ def visualize(data, anomalies, title):
 
     # Generate title to show window count and anomaly window
     ax1.title.set_text(title)
-    ax1.plot(np.arange(data.size), data, color='blue', zorder=-1)
+    ax1.plot(np.arange(data.size), data, color='blue', zorder=1)
     # Add anomaly markers
     for anomaly_index in anomalies:
-        ax1.scatter(anomaly_index, data[anomaly_index], marker='x', color='red', zorder=1)
+        ax1.scatter(anomaly_index, data[anomaly_index], marker='x', color='red', zorder=2)
     plt.tight_layout() # avoid overlapping plot titles
-    fig.savefig('taxi_nyc_numenta_data/taxi_data.png')
+    fn = '{}/taxi_data.png'.format(folder)
+    fig.savefig(fn)
+    tc.green('Created {}'.format(fn))
+
     # plt.show()
 
 
 window_size = 100
 anomalies = [5942, 7183, 8526, 8834, 10080] # indecies of anomaly timeseries datapoints
 # Load timeseries data from csv file
-data = pd.read_csv('data/Numenta/data/realKnownCause/nyc_taxi.csv').values[:, 1]
+data = pd.read_csv('data/Numenta/data/realKnownCause/nyc_taxi.csv').value.values
 visualize(data, anomalies, 'NYC Taxi data')
+labels = np.zeros(len(data), dtype=int)
+for ai in anomalies:
+    labels[ai] = 1
 
-# anomaly_windows = [117,118, 142,143, 169,170, 175,176, 200,201] # two windows per anomaly
-anomaly_windows = utils.anomalies_index_to_window_index(anomalies, window_size)
-print(anomaly_windows)
 
 # Generate features
-processor = dataProcessor.DataProcessor(window_size, anomaly_windows)
-features_file_name = 'taxi_nyc_numenta_data/taxi_features'
-features = processor.reduce_arma(data, features_file_name)
-features = pd.read_csv('{}.csv'.format(features_file_name)).values
-processor.visualize_features(features, features_file_name)
+processor = dataProcessor.DataProcessor()
+fn = '{}/taxi.csv'.format(folder)
+data = processor.generate_features(
+    data, 
+    labels,
+    window_size,
+    fn
+)
+# data = pd.read_csv(fn)
+processor.visualize_features(data, fn, method='TSNE')
 
 # Detect anomalies
-outliers_fraction = 10 / 205
+features = data.drop(['is_anomaly', 'window_label'], axis=1).values
+outliers_fraction = data.is_anomaly.mean()
 detector = anomalyDetector.AnomalyDetector(
     outliers_fraction, 
     window_size,
-    'taxi_nyc_numenta_data/anomalies'
+    '{}/anomalies.png'.format(folder)
 )
 detector.detect_anomalies(features)
